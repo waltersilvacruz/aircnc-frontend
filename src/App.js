ba451bbfe8104cd0fbb08f961788c6cf43ebd19e
Walter Cruz <walter@tnx.com.br>
2019-10-03 11:31:33 -0400

    Concluindo o dashboard do frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="container">
-      <img src={logo} alt=""/>
-      <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
-        </p>
-        <form>
-          <label htmlFor="email">E-MAIL*</label>
-          <input type="email" id="email" placeholder="Seu melhor email"/>
-          <button type="submit" className="btn">Entrar</button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <img src={logo} alt=""/>
+            <div className="content">
+                <Routes />        
+            </div>
+        </div>
+    );
 }
 
 export default App;
